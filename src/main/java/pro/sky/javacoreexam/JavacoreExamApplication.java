4cf2f5020575e6d825832dcf9487549d24bb10bf
whoisxanny <tuffx4nny@gmail.com>
2024-01-26 14:21:50 +0500

    Initials

---
+++ src/main/java/pro/sky/javacoreexam/JavacoreExamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.javacoreexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacoreExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacoreExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/javacoreexam/JavacoreExamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.javacoreexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacoreExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
